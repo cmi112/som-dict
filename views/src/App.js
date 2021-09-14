ac5ae56563c8bff166d047faed50d18409b4145a
cmi112 <cabdiqanimuuse7@gmail.com>
2021-09-14 20:16:39 +0200

    new folder in react app

---
+++ views/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import PostWords from './components/PostWords';
+
+function App() {
+  return (
+    <div className="App">
+    <h1>Hello</h1>
+    <PostWords/>
+    </div>
+  );
+}
+
+export default App;
